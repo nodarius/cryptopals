ac120b6c704f6a68b3f5d4184bcabf61fe79118a
nodarius <n0dari@yahoo.com>
2015-02-25 19:34:52 +0400

    challenge 15 (too easy)

--- 15.py
+++ 15.py
@@ -3,14 +3,6 @@
 class IncorrectPadding(Exception):
     pass
 
-def pad(text, size):
-    diff = size - len(text) % size
-    for i in range(diff):
-#        print(chr(diff).encode())
-        text += chr(diff).encode()
-    return text
-
-
 def unpad_exc(string):
     size = string[-1]
     if size == 0 or size >= len(string):
