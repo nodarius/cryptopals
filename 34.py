3c8f6facbf11d752e907022d889419ac338b6f5c
nodarius <n0dari@yahoo.com>
2015-04-03 21:35:12 +0400

    MITM key-fixing attack on DH with parameter injection

--- 34.py
+++ 34.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 
 import random
-
-def dh():
-    p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
-    g = 2
-
-    a = random.randint(0, p) % p
-    A = pow(g, a, p)
-    b = random.randint(0, p) % p
-    B = pow(g, b, p)
-
-    s = pow(B, a, p)
-    s1 = pow(A, b, p)
-    if s1 != s:
-        print("error")
-    else:
-        print("ok")
-    return s
-
+from Crypto import Random as crandom
+from Crypto.Cipher import AES
 from hashlib import sha1
+
+def pad(text, blocksize):
+    n = blocksize - len(text) % blocksize
+    for i in range(0, n):
+        text += chr(n).encode();
+    return text
+
+def unpad(string):
+    size = string[-1]
+    return string[:-size]
+
+def int_to_hash(num):
+    return sha1(str(num).encode()).digest()[:16]
+
 class A:
     def __init__(self):
         self.p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
@@ -39,12 +37,18 @@ class A:
     def get_g(self):
         return self.g
 
-    def send2(self):
-        #hsh = sha1.digest(str(self.s).encode()).digest()
-        pass
     def get_session_key(self):
         s = pow(self.B, self.a, self.p)
+        s = int_to_hash(s)
         return s
+
+    def get_message(self):
+        self.message = b'All hail Totti'
+        key = self.get_session_key()
+        iv = crandom.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        enc = cipher.encrypt(pad(self.message, AES.block_size)) + iv
+        return enc
 
 class B:
     def __init__(self):
@@ -66,25 +70,56 @@ class B:
 
     def get_session_key(self):
         s = pow(self.A, self.b, self.p)
+        s = int_to_hash(s)
         return s
 
+    def receive_message(self, message):
+        key = self.get_session_key()
+        iv = message[-16:]
+        message = message[:-16]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        dec = unpad(cipher.decrypt(message))
+        self.message = dec
+
+    def get_message(self):
+        return self.message
+    
 class M:
     def __init__(self):
         pass
 
+def run_mitm():
+    print("Running MITM")
+    a, b = A(), B()
+    b.set_p(a.get_p())
+    b.set_g(a.get_g())
+    b.set_A(a.get_p())
+    bb = b.get_B()
+    a.set_B(a.get_p())
+    msg_a = a.get_message()
+    b.receive_message(msg_a)
+    msg_b = b.get_message()
 
-def run():
+    key = int_to_hash(0)
+    iv = msg_a[-16:]
+    msg_a = msg_a[:-16]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    dec = unpad(cipher.decrypt(msg_a))
+    print("Decripted: %s" % dec)
+
+def run_normal():
+    print("Running normal..", end=' ')
     a, b = A(), B()
     b.set_A(a.get_A())
     b.set_p(a.get_p())
     b.set_g(a.get_g())
     a.set_B(b.get_B())
-    s = b.get_session_key()
-    print(s)
-    s1 = a.get_session_key()
-    print(s1)
-    if s1 == s:
-        print('ok')
+    session = None
+    if b.get_session_key() == a.get_session_key():
+        session = int_to_hash(a.get_session_key())
+    b.receive_message(a.get_message())
+    print("Done.")
 
 if __name__ == '__main__':
-    run()
+    run_normal()
+    run_mitm()
