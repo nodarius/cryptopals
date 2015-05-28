a1a500a379edc1f6742f68d8892cad98b1f2ec41
nodarius <n0dari@yahoo.com>
2015-05-28 19:21:42 +0400

    invmod

--- 39.py
+++ 39.py
@@ -15,10 +15,27 @@ def generate_prime(low, high):
             return num
     return None
 
+def invmod(x, N):
+    for i in range(0, x):
+        rem = (i * N + 1) % x
+        if rem == 0:
+            return (i * N + 1) / x
+    return None
 
 def run():
-    prime = generate_prime(1000, 20000)
-    print(prime)
+    p = generate_prime(1000, 20000)
+    q = generate_prime(1000, 20000)
+    n = p * q
+
+    et = (p - 1) * (q - 1)
+    e = 3
+    inv = invmod(17, 3120)
+    print(inv)
+    print(p)
+    print(q)
+    print(n)
+    print(et)
+
 
 
 if __name__ == '__main__':
